5b08aaafe4fc7106d0dba25888d1e16272dc7a96
Felipe <faraguti@byui.edu>
2022-01-28 13:12:06 -0700

    login/pw fixed

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
     res.send("Hello Browser");
+});
+
+app.post('/login', (req,res) =>{
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName =="faraguti" && req.body.password=="1234"){
+        res.send("Welcome!")
+    } else{
+        res.send("Who are you?");
+    }
 });
 
 app.listen(port, ()=>{});
