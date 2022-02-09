321ac54a00983ba52a5348d182d1b89e12a720b7
Felipe <faraguti@byui.edu>
2022-02-08 17:15:41 -0700

    server v2.1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const https = require('https')
 const fs = require('fs')
+const app = express()
 // const md5 = require('md5');
 // const bodyParser = require('body-parser');
 
