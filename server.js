ea885d4184aee1d5789245bd6283d58b47f8507b
faraguti <faraguti@gmail.com>
2022-02-08 17:07:49 -0700

    https merged to server.js

--- server.js
+++ server.js
@@ -2,24 +2,39 @@
 const md5 = require('md5');
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https')
+const fs = require('fs')
 
-const port = 3000;
 
-const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello HTTPS!')
+  })
+  
+  https.createServer({
+      key: fs.readFileSync('/Users/faraguti/server.key'),
+      cert: fs.readFileSync('/Users/faraguti/server.cert')
+  }, app).listen(3000, () => {
+    console.log('Listening...')
+  })
 
-app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-    res.send("Hello Browser");
-});
+// const port = 3000;
 
-app.post('/login', (req,res) =>{
-    console.log(JSON.stringify(req.body));
-    if(req.body.userName =="faraguti" && md5(req.body.password)=="81dc9bdb52d04dc20036dbd8313ed055"){
-        res.send("Welcome!")
-    } else{
-        res.send("Who are you?");
-    }
-});
+// const app = express();
 
-app.listen(port, ()=>{});
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res)=>{
+//     res.send("Hello Browser");
+// });
+
+// app.post('/login', (req,res) =>{
+//     console.log(JSON.stringify(req.body));
+//     if(req.body.userName =="faraguti" && md5(req.body.password)=="81dc9bdb52d04dc20036dbd8313ed055"){
+//         res.send("Welcome!")
+//     } else{
+//         res.send("Who are you?");
+//     }
+// });
+
+// app.listen(port, ()=>{});
