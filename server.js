57c21060b4040982f30840cf4f85412d97e751ef
Felipe <faraguti@byui.edu>
2022-03-15 16:40:22 -0600

    invalid login attempts added

--- server.js
+++ server.js
@@ -5,8 +5,28 @@ const app = express()
 const port = 443
 const md5 = require('md5');
 const bodyParser = require('body-parser');
+var invalidLoginAttempts=0;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+app.post('/login', (req,res) =>{
+    console.log(JSON.stringify(req.body));
+    if(invalidLoginAttempts>=5){
+        res.status(401);//unauthorized
+    }
+    console.log("Here is the password " + req.body.password)
+    if(req.body.userName =="faraguti" && md5(req.body.password)=="ed8d20de1864bc073df52d0fe74b9031"){
+        res.send("Welcome!")
+    } else{
+        invalidLoginAttempts++;
+        console.log(invalidLoginAttempts+" invalid attempts")
+        res.status(401);//unauthorized
+        res.send("Who are you?")
+    }
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello HTTPS!')
@@ -19,18 +39,5 @@ app.get('/', (req, res) => {
   }, app).listen(port, () => {
     console.log('Listening...')
   })
-
-
-app.use(bodyParser.json());
-
-app.post('/login', (req,res) =>{
-    console.log(JSON.stringify(req.body));
-    console.log("Here is the password " + req.body.password)
-    if(req.body.userName =="faraguti" && md5(req.body.password)=="ed8d20de1864bc073df52d0fe74b9031"){
-        res.send("Welcome!")
-    } else{
-        res.send("Who are you?");
-    }
-});
 
 // app.listen(port, ()=>{});
