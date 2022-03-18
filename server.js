e088a2f492e26e929f9784b15852c801b9c2c754
faraguti <faraguti@gmail.com>
2022-03-17 20:27:08 -0600

    if else added

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ let invalidLoginAttempts=0;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello HTTPS!')
+  })
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
     if(invalidLoginAttempts>=5){
         res.status(401);//unauthorized
+        res.send("Max attempts reached");
     }
     // console.log("Here is the password " + req.body.password)
     else if(req.body.userName =="faraguti" && md5(req.body.password)=="ed8d20de1864bc073df52d0fe74b9031"){
@@ -27,17 +31,10 @@ app.post('/login', (req,res) =>{
     }
 });
 
-
-app.get('/', (req, res) => {
-    res.send('Hello HTTPS!')
-  })
-  
-  https.createServer({
-      key: fs.readFileSync('server.key'),
-      cert: fs.readFileSync('server.cert'),
-      passphrase:'P@ssw0rd'
-  }, app).listen(port, () => {
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+    passphrase:'P@ssw0rd'
+}, app).listen(port, () => {
     console.log('Listening...')
-  })
-
-// app.listen(port, ()=>{});
+})
